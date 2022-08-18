58729afc794cb769bed21ea1997e12b761929630
chnnxyz <s@chnn.xyz>
2022-08-17 19:55:59 -0500

    py act 1

---
+++ class_activities/03_python/01_variable_types.py
@@ -0,0 +1,11 @@
+#Crea un string que diga "Hola Mundo"
+salu2: str = "Hola Mundo"
+#Define una variable numerica n
+n:int = 5
+#Extrae en una variable los primeros n elementos
+salu2_corta2 = salu2[:n]
+#Convierte a mayusculas y minúsculas
+print(salu2_corta2.upper())
+print(salu2_corta2.lower())
+# Divide la longitud entre 7
+print(len(salu2_corta2)/7)
