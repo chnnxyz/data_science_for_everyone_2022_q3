bc21682b5898c377761d099ebd2a9865a9ad337b
chnnxyz <s@chnn.xyz>
2022-10-05 16:37:29 -0500

    sql activities

---
+++ class_activities/07_SQL_API/01_Create_table/create_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE lungcancer(
+    GENDER varchar,
+    AGE integer,
+    SMOKING integer,
+    YELLOW_FINGERS integer,
+    ANXIETY integer,
+    PEER_PRESSURE integer,
+    CHRONIC_DISEASE integer,
+    FATIGUE integer,
+    ALLERGY integer,
+    WHEEZING integer,
+    ALCOHOL_CONSUMING integer,
+    COUGHING integer,
+    SHORTNESS_OF_BREATH integer,
+    SWALLOWING_DIFFICULTY integer,
+    CHEST_PAIN integer,
+    LUNG_CANCER varchar
+)
